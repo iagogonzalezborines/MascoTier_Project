b23c4d4f954efddd3ff89f3640685045ab37d425
iagogonzalezborines <iagogonzalezborines@gmail.com>
2024-02-02 13:23:47 +0100

    updateOwner

--- MascoDocker/src/Classes/Owner.php
+++ MascoDocker/src/Classes/Owner.php
@@ -1,4 +1,8 @@
-<?php class Owner extends User { private $contactNumber; private $pets; public function __construct($username, $email, $password, $contactNumber) { parent::__construct($username, $email, $password);
+<?php 
+
+include '/DataBase/connectToDb';
+include '/DataBase/executeQuery';
+class Owner extends User { private $contactNumber; private $pets; public function __construct($username, $email, $password, $contactNumber) { parent::__construct($username, $email, $password);
         $this->contactNumber = $contactNumber;
         $this->pets = [];
     }
@@ -13,8 +17,12 @@
         $this->pets[] = $pet;
     }
 
-    public function getPets()
+    public function getPets(userId)
     {
+      $dbh = connectToDb();
+      $query = 'SELECT * FROM pet WHERE user_id = '$userId'';
+      $pets = executeQuery($dbh, $query);
+      //Turn resultset to type
         return $this->pets;
     }
 }
