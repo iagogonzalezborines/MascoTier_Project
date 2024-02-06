5202e667fc39dea32f1dc72d0d31bdf81316399e
iagogonzalezborines <iagogonzalezborines@gmail.com>
2024-02-06 08:48:42 +0100

    Updated user class

--- MascoDocker/src/Classes/User.php
+++ MascoDocker/src/Classes/User.php
@@ -2,7 +2,8 @@
 
 require_once '../DataBase/dataBase.php';
 require_once 'PHPMailer/PHPMailer.php';
-class User {
+class User
+{
     private int $userId;
     private string $username;
     private string $email;
@@ -13,7 +14,8 @@ class User {
     private bool $verified;
 
 
-    public function __construct(int $userId,string $username, string $email, string $password, string $phone, bool $hasPlace, string $area, bool $verified) {
+    public function __construct(int $userId, string $username, string $email, string $password, string $phone, bool $hasPlace, string $area, bool $verified)
+    {
         $this->userId = $userId;
         $this->username = $username;
         $this->email = $email;
@@ -22,77 +24,83 @@ class User {
         $this->hasPlace = $hasPlace;
         $this->area = $area;
         $this->verified = $verified;
-
     }
 
-    public function getUserId(): int {
+    public function getUserId(): int
+    {
         return $this->userId;
     }
 
-    public function getUsername(): string {
+    public function getUsername(): string
+    {
         return $this->username;
     }
 
-    public function getEmail(): string {
+    public function getEmail(): string
+    {
         return $this->email;
     }
 
-    public function setPassword(string $password): void {
+    public function setPassword(string $password): void
+    {
         $this->password = $password;
     }
 
-    public function transformEmailToUsername(): void {
+    public function transformEmailToUsername(): void
+    {
         $this->username = strstr($this->email, '@', true);
     }
 
-    public function hashPassword(): string {
+    public function hashPassword(): string
+    {
         return $hashedPassword = password_hash($this->password, PASSWORD_DEFAULT);
     }
 
-    public function saveUser() {
+    public function saveUser()
+    {
         $hashedPassword = $this->hashPassword();
-          $query = "INSERT INTO users (user_id, email, pwd, phone, has_place, area, verified) VALUES ('$this->userId', '$this->email', '$hashedPassword', '$this->phone', '$this->hasPlace', '$this->area', '$this->verified')";
-            $db = dataBase::getInstance();
-            $db->connectToDatabase();
-            $db->executeQuery($query);
-            $db->disconnectFromDatabase();
+        $query = "INSERT INTO users (user_id, email, pwd, phone, has_place, area, verified) VALUES ('$this->userId', '$this->email', '$hashedPassword', '$this->phone', '$this->hasPlace', '$this->area', '$this->verified')";
+        $db = dataBase::getInstance();
+        $db->connectToDatabase();
+        $db->executeQuery($query);
+        $db->disconnectFromDatabase();
     }
- 
-    public function transformResultSetIntoUserArray($result){
+
+    public function transformResultSetIntoUserArray($result)
+    {
         $userArray = [];
         while ($row = $result->fetch_assoc()) {
             $userArray[] = $row;
         }
         return $userArray;
     }
-    public function getUserDataFromDataBase(){
+    public function getUserDataFromDataBase()
+    {
         $db = dataBase::getInstance();
         $db->connectToDatabase();
         $query = "SELECT * FROM users WHERE email = '$this->email'";
         $result = $db->executeQuery($query);
         $db->disconnectFromDatabase();
-        return $this->transformResultSetIntoUserArray($result);   
+        return $this->transformResultSetIntoUserArray($result);
     }
-    public function setUserToVerified($bool){
-      if($bool == true) {
-        $verified = true;
-        $query = "UPDATE users SET verified = 1 WHERE email = '$this->email'";
-        $db = dataBase::getInstance();
-        $db->connectToDatabase();
-        $db->executeQuery($query);
-        $db->disconnectFromDatabase();
-              
-
-      } else {
-        $verified = false;
-        $query = "UPDATE users SET verified = 0 WHERE email = '$this->email'";
-      }
+    public function setUserToVerified($bool)
+    {
+        if ($bool == true) {
+            $verified = true;
+            $query = "UPDATE users SET verified = 1 WHERE email = '$this->email'";
+            $db = dataBase::getInstance();
+            $db->connectToDatabase();
+            $db->executeQuery($query);
+            $db->disconnectFromDatabase();
+        } else {
+            $verified = false;
+            $query = "UPDATE users SET verified = 0 WHERE email = '$this->email'";
+        }
     }
-}  
-   /* public function verifyUserPhpMailer(){}
+}
+/* public function verifyUserPhpMailer(){}
       function which sends and recieves an email from the user to confirmed its verified if it is call 
       setUserToVerified          $mail->Username = '
    
 }
 */
-?>
