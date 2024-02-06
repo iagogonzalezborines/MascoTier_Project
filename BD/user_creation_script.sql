4dbc5ebfd9f3c693be63ce0392137b8b37dd6895
iagogonzalezborines <iagogonzalezborines@gmail.com>
2024-02-06 09:04:36 +0100

    Updated user script still to be modified

--- BD/user_creation_script.sql
+++ BD/user_creation_script.sql
@@ -1,3 +1,5 @@
 CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';
-
 GRANT SELECT, INSERT, UPDATE, DELETE ON `database_name`.`table_name` TO 'user'@'localhost';
+
+-- Path: BD/user_deletion_script.sql
+
