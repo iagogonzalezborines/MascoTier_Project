9802b5f2c2f19d863a9eb0db40120ba3aaf1ea1b
joeljnq <joelxdnq@gmail.com>
2024-02-06 17:00:26 +0100

    signup_ created

--- MascoDocker/src/Templates/index.php
+++ MascoDocker/src/Templates/index.php
@@ -1,5 +1,6 @@
 <?php 
 
+
 require_once 'vendor/autoload.php';
 
 
@@ -25,6 +26,7 @@ require_once 'vendor/autoload.php';
 </body>
 </html>
 <?php
+var_dump($_POST);
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $_POST["email"];
         $password = $_POST["password"];
