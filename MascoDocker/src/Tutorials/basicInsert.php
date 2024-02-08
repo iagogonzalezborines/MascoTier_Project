9df824e776c36c785609c7431b034c7f89170e4b
Pedro Campelo <118432032+WorkPedroCampelo@users.noreply.github.com>
2024-02-08 14:08:06 +0100

    add comments

--- MascoDocker/src/Tutorials/basicInsert.php
+++ MascoDocker/src/Tutorials/basicInsert.php
@@ -4,7 +4,7 @@ include_once '../DataBase/dataBase.php';
 $pdo= dataBase::getInstance();
 $db = $pdo->connectToDatabase();
 
-$statement = $db->prepare("INSERT INTO owner(user_id) VALUES (14)");
+$statement = $db->prepare("INSERT INTO owner(user_id) VALUES (14)");//this number is the owner id so it has to be changed every time we use this file
 
 try {
     $statement->execute();
