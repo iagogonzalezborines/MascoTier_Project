e3e5ffa63d7792137b319a31211529bbe7ea8894
iagogonzalezborines <iagogonzalezborines@gmail.com>
2024-02-15 09:08:57 +0100

    quieckfix

--- MascoDocker/src/Controller/login.php
+++ MascoDocker/src/Controller/login.php
@@ -4,36 +4,35 @@
  * This file is the login controller for the application.
  */
 
- require_once '../DataBase/dataBase.php';
+require_once '../DataBase/dataBase.php';
 
- if($_SERVER['REQUEST_METHOD'] === 'POST') {
-     // Retrieve login information from login.html
-     $email = $_POST['email'];
-     if(filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-         echo "LOGIN FAILED EMAIL NOT VALID";
-         return;
-     }
-     else{
-     $password = $_POST['password'];
-     $db = dataBase::getInstance();
-     $dbh = $db->connectToDatabase();
-     $query = "SELECT * FROM users WHERE email = :email";
-     $result = $db->executeQuery($query, array(':email' => $email));
-     $arrayResult = $db -> transformResultSetIntoUserArray($result);
-     if (count($arrayResult) > 0) {
-         $hashedPassword = $arrayResult[0]['password'];
-         if (password_verify($password, $hashedPassword)) {
-            //Start session
-            session_start();
-             $_SESSION['email'] = $arrayResult[0];
-             echo "LOGIN SUCCESSFUL";
-         } else {
-             echo "LOGIN FAILED PASSWORD INCORRECT";
-         }
-     } else {
-         echo "LOGIN FAILED USER NOT FOUND";
-     }
-     $db->disconnectFromDatabase();
- }
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve login information from login.html
+    $email = $_POST['email'];
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+        echo "LOGIN FAILED EMAIL NOT VALID";
+        return;
+    } else {
+        $db = dataBase::getInstance();
+        $dbh = $db->connectToDatabase();
+        $query = "SELECT * FROM users WHERE email = :email";
+        $result = $db->executeQuery($query, array(':email' => $email));
+        $arrayResult = $db->transformResultSetIntoUserArray($result);
+        if (count($arrayResult) > 0) {
+            $password = $_POST['password'];
+            $hashedPassword = $arrayResult[0]['password'];
+            if (password_verify($password, $hashedPassword)) {
+                // Start session
+                session_start();
+                $_SESSION['email'] = $arrayResult[0];
+                echo "LOGIN SUCCESSFUL";
+            } else {
+                echo "LOGIN FAILED PASSWORD INCORRECT";
+            }
+        } else {
+            echo "LOGIN FAILED USER NOT FOUND";
+        }
+        $db->disconnectFromDatabase();
+    }
 }
 ?>
