c1f086675bd27b1b815cd31759821f74926b9c96
JOEL NINAHUAMAN <joelxdnq@gmail.com>
2024-02-15 11:08:53 +0100

    updated index... pop-up feature added

---
+++ MascoDocker/src/Templates/scripts/login-pop-up.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("pop-up");
+
+button.addEventListener("click",()=>{
+   
+        // Abrir una nueva ventana con la página HTML
+        var popup = window.open("login.html", "popup", "width=600,height=400");
+      
+});
