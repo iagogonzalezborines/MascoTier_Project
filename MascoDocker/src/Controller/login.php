361a1343fa99639d99ca0cc4ab8f7bcb428f01f0
iagogonzalezborines <iagogonzalezborines@gmail.com>
2024-02-22 10:10:57 +0100

    fixed login

--- MascoDocker/src/Controller/login.php
+++ MascoDocker/src/Controller/login.php
@@ -1,45 +1,16 @@
 <?php
-session_start();
+
 /**
  * This file is the login controller for the application.
  */
 
 require_once '../DataBase/dataBase.php';
 
-function logIn(){
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $email = $_POST['email'];
         $password = $_POST['password'];
-        
-        if(!empty($email) && !empty($password)){
-            $db = dataBase::getInstance();
-            $dbh = $db->connectToDatabase();
-        
-            $query = "SELECT * FROM users WHERE email = ?";
-            $result = $db->executeQuery($query, array($email));
-            $arrayResult = $db->transformResultSetIntoUserArray($result);
-        
-            if (count($arrayResult) === 0) {
-                $db->disconnectFromDatabase();
-                return false;
-            }
-            
-            $hashedPassword = $arrayResult[0]["pwd"];
-            if (!password_verify($password, $hashedPassword)) {
-                $db->disconnectFromDatabase();
-                return false;
-            }
-            
-            $_SESSION['email'] = $arrayResult[0];
-            $db->disconnectFromDatabase();
-            session_start();
-            $_SESSION['email'] = $arrayResult[0];
-            
-            return true;
-        }
+        $db = dataBase::getInstance();
+        $db->connectToDatabase();
+        $validLogin = $db -> logIn($email, $password, $db);
     }
     
-    return false;
-}
-
-$validLogin = logIn();
