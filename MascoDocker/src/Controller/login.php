a5a6a4320fd673b9208ab3920e71bf354946723e
Pedro Campelo <118432032+WorkPedroCampelo@users.noreply.github.com>
2024-02-26 09:43:25 +0100

    header location not working on login

--- MascoDocker/src/Controller/login.php
+++ MascoDocker/src/Controller/login.php
@@ -13,7 +13,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $db->connectToDatabase();
     if (!empty($email) && !empty($password)) {
 
-
         $query = "SELECT * FROM users WHERE email = ?";
         $result = $db->executeQuery($query, array($email));
         $arrayResult = $db->transformResultSetIntoUserArray($result);
@@ -22,29 +21,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $db->disconnectFromDatabase();
         
         }
-        var_dump($arrayResult);
         $hashedPassword = $arrayResult[0]["pwd"];
         
         if (!password_verify($password, $hashedPassword) && $email == $arrayResult[0]["email"])  {
             $db->disconnectFromDatabase();
         echo " fail";
         }
+
         $_SESSION['email'] = $arrayResult[0]["email"];
-        echo "login sucessfull";
         $db->disconnectFromDatabase();
+        header('Location: ../Templates/carerList.php');
     }
 
-    //   $validLogin = $db -> logIn($email, $password, $db);
 }
 
-//Temporal alerts
-/*
-if ($validLogin) {
-    echo '<div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">';
-    echo '<span class="font-medium">Success alert!</span> You have successfully logged in.';
-    echo '</div>';
-} else {
-    echo '<div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">';
-    echo '<span class="font-medium">Error alert!</span> Change a few things up and try submitting again.';
-    echo '</div>';
-}*/
+
