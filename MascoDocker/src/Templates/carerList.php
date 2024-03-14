3d433acde573be9f19edf79afb48e1903118d30f
Pedro Campelo <118432032+WorkPedroCampelo@users.noreply.github.com>
2024-03-14 14:07:13 +0100

    update carerList

--- MascoDocker/src/Templates/carerList.php
+++ MascoDocker/src/Templates/carerList.php
@@ -15,38 +15,46 @@ require_once '../DataBase/dataBase.php';
         .trFeedUser {
             text-align: center;
         }
+
+        body {
+            background-color: #f5efd4;
+            
+        }
+
+        nav {
+            
+            width: 100%;
+            min-height: 175px !important;
+        }
+        .logo {
+            width: 150px;
+
+        }
     </style>
 </head>
 
-<body class="bg-gray-800 p-4">
-    <header>
-        <nav  id="navbar">
-            <div class="flex items-center justify-between">
-                <!-- temporal Logo -->
-                <div class="flex items-center mr-10">
-                    <img class="h-20 w-20" src="media/logo.svg" alt="Logo" />
-                    <a href="/Templates/carerList"><span class="text-white font-bold text-lg">MASCOTIER</span></a>
+<body class="">
+    <header class="pt-11">
+        <nav id="navbar">
+            <div class="flex items-center justify-between h-20 w-full pr-32 p-10 mb-6">
+
+                <img src="media/logo-removebg-preview.png" alt="" class="logo ml-28">
+
+                <div class="hidden md:block">
+                    <div class="ml-10 flex items-baseline space-x-4">
+                        <!-- Add navigation links here -->
+                    </div>
                 </div>
+                <button class="md:hidden bg-transparent text-white hover:text-gray-300 focus:outline-none">
+                    <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
+                        </path>
+                    </svg>
+                </button>
+                
                 <!-- Links del navbar -->
-                <ul class="flex items-center w-1/2 justify-start p-5">
-                    <li class="mr-6">
-                        <a href="#" class="text-gray-100 hover:text-blue-400">Item</a>
-                    </li>
-                    <li class="mr-6">
-                        <a href="#" class="text-gray-100 hover:text-blue-400">Item</a>
-                    </li>
-                    <li class="mr-6">
-                        <a href="#" class="text-gray-100 hover:text-blue-400">Item</a>
-                    </li>
-                    <li class="mr-6">
-                        <a href="#" class="text-gray-100 hover:text-blue-400">Item</a>
-                    </li>
-                    <li class="mr-6">
-                        <a href="#" class="text-gray-100 hover:text-blue-400">Item</a>
-                    </li>
-                    <li class="mr-6">
-                        <a href="#" class="text-gray-100 hover:text-blue-400">Item</a>
-                    </li>
+                <ul class="flex items-end w-1/2 justify-start p-5">
+
                     <li class="">
                         <a href="../Controller/profileCarer.php" class="btn-blue font-bold py-2 px-4 rounded-full focus:outline-none p-2 text-xl">Perfil</a>
                     </li>
@@ -58,13 +66,13 @@ require_once '../DataBase/dataBase.php';
 
             <!--ENSEÑAR ESTO SOLO SI EL USUARIO NO ESTÁ VERIFICADO AÚN!!-->
             <div class=" m-auto w-5/6 bg-slate-100 text-black p-1 flex justify-center items-center ">
-                 <p> Aún no te has verificado? Hazlo ahora y consigue todas las funcionalidades de Mascotier de manera gratuíta </p>
-                 <button type="button" class=" text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-4 "><a href="/templates">Verificarme ahora!</a></button>
+                <p> Aún no te has verificado? Hazlo ahora y consigue todas las funcionalidades de Mascotier de manera gratuíta </p>
+                <button type="button" class=" text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-4 "><a href="/templates">Verificarme ahora!</a></button>
 
-        </div>
+            </div>
 
         </nav>
-        <h1 class="text-white text-center text-5xl mt-5">Cuidadores disponibles</h1>
+        <h1 class="text-black text-center text-5xl mt-5">Cuidadores disponibles</h1>
     </header>
 
 
